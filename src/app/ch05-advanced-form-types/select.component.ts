c9b3e7eb1f658341533d7a2da1d98f1fe5b47c38
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-11-23 10:52:17 -0500

    add - select example

--- src/app/ch05-advanced-form-types/select.component.ts
+++ src/app/ch05-advanced-form-types/select.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-select',
   template: `
-    <p>
-      select works!
-    </p>
+    <form [formGroup]="form" (ngSubmit)="onSubmit()">
+      <!-- When setting the options of the select, we have to make sure to provide the value attribute for
+          Angular to use and assign to the control value. Note that when using the value attribute the value
+          returned always is a string type. In a later chapter, we will cover in detail how to create selects
+          dynamically from async data.
+      -->
+      <select name="memory" id="memory" formControlName="memory">
+        <option value="16">16 Gigabytes</option>
+        <option value="32">32 Gigabytes</option>
+        <option value="64">64 Gigabytes</option>
+        <option value="128">128 Gigabytes</option>
+      </select>
+    </form>
+    <pre>{{ form.value | json }}</pre>
   `,
-  styles: [
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SelectComponent implements OnInit {
+export class SelectComponent {
+  form = new FormGroup({
+    memory: new FormControl('32'),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    console.log('Submitted: ', this.form);
   }
-
 }
