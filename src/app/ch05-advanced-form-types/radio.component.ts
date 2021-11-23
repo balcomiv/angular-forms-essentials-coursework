d4cc719795ea8f5a87bf8e5e679224c1306b4855
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-11-23 14:22:37 -0500

    add - radio component code

--- src/app/ch05-advanced-form-types/radio.component.ts
+++ src/app/ch05-advanced-form-types/radio.component.ts
@@ -1,13 +1,53 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-radio',
-  template: ` works `,
+  template: `
+    <form [formGroup]="form">
+      <span class="label">Region (type="radio")</span>
+      <label>
+        <input
+          formControlName="radio"
+          type="radio"
+          name="region"
+          value="north-america"
+        />
+        North America
+      </label>
+      <label>
+        <input
+          formControlName="radio"
+          type="radio"
+          name="region"
+          value="south-america"
+        />
+        South America
+      </label>
+      <label>
+        <input
+          formControlName="radio"
+          type="radio"
+          name="region"
+          value="europe"
+        />
+        Europe
+      </label>
+    </form>
+
+    <pre>{{ form.value | json }}</pre>
+  `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RadioComponent implements OnInit {
-  constructor() {}
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      radio: ['south-america'],
+    });
+  }
 
   ngOnInit(): void {}
 }
